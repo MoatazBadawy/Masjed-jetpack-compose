6ed4b86b07e62a00db3d2a84d17676474b68cca6
MoatazBadawy <moataz.egy101@gmail.com>
2023-05-28 19:56:14 +0300

    fix: update masjed viewmodel in "getAllMasjeds" function.

--- ui/viewmodel/src/main/java/com/moataz/masjed/ui/viewmodel/masjeds/MasjedViewModel.kt
+++ ui/viewmodel/src/main/java/com/moataz/masjed/ui/viewmodel/masjeds/MasjedViewModel.kt
@@ -2,12 +2,9 @@ package com.moataz.masjed.ui.viewmodel.masjeds
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.moataz.masjed.domain.usecases.AddNewMasjedUsecase
 import com.moataz.masjed.domain.usecases.GetAllMasjedsUsecase
 import com.moataz.masjed.domain.usecases.utils.MasjedResult
-import com.moataz.masjed.ui.viewmodel.models.MasjedUI
 import com.moataz.masjed.ui.viewmodel.models.MasjedsUIState
-import com.moataz.masjed.ui.viewmodel.models.mapper.toMasjed
 import com.moataz.masjed.ui.viewmodel.models.mapper.toMasjeds
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -28,68 +25,59 @@ class MasjedViewModel @Inject constructor(
 
     private fun getAllMasjeds() {
         viewModelScope.launch {
-            try {
-                getAllMasjedsUsecase().collect { masjedResult ->
-                    when (masjedResult) {
-                        is MasjedResult.Loading -> {
-                            _masjedsUiState.update { it.copy(isLoading = true) }
-                        }
+            getAllMasjedsUsecase().collect { masjedResult ->
+                when (masjedResult) {
+                    is MasjedResult.Loading -> {
+                        _masjedsUiState.update { it.copy(isLoading = true) }
+                    }
 
-                        is MasjedResult.Success -> {
-                            _masjedsUiState.update {
-                                it.copy(
-                                    masjeds = masjedResult.masjeds.toMasjeds(),
-                                    isLoading = false,
-                                    isError = false,
-                                    isSuccessful = true
-                                )
-                            }
+                    is MasjedResult.Success -> {
+                        _masjedsUiState.update {
+                            it.copy(
+                                masjeds = masjedResult.masjeds.toMasjeds(),
+                                isLoading = false,
+                                isError = false,
+                                isSuccessful = true
+                            )
                         }
+                    }
 
-                        is MasjedResult.EmptyResult -> {
-                            _masjedsUiState.update {
-                                it.copy(
-                                    isLoading = false,
-                                    isError = false,
-                                    isSuccessful = true
-                                )
-                            }
+                    is MasjedResult.EmptyResult -> {
+                        _masjedsUiState.update {
+                            it.copy(
+                                isLoading = false,
+                                isError = false,
+                                isSuccessful = true,
+                                masjeds = emptyList()
+                            )
                         }
+                    }
 
-                        is MasjedResult.NoInternet -> {
-                            _masjedsUiState.update {
-                                it.copy(
-                                    masjeds = masjedResult.cachedMasjeds.toMasjeds(),
-                                    isLoading = false,
-                                    isError = false,
-                                    isSuccessful = true
-                                )
-                            }
+                    is MasjedResult.NoInternet -> {
+                        _masjedsUiState.update {
+                            it.copy(
+                                masjeds = masjedResult.cachedMasjeds.toMasjeds(),
+                                isLoading = false,
+                                isError = false,
+                                isSuccessful = true
+                            )
                         }
+                    }
 
-                        is MasjedResult.Failure -> {
-                            _masjedsUiState.update {
-                                it.copy(
-                                    isError = true,
-                                    isLoading = false,
-                                    isSuccessful = false,
-                                    masjeds = listOf()
-                                )
-                            }
+                    is MasjedResult.Failure -> {
+                        _masjedsUiState.update {
+                            it.copy(
+                                isError = true,
+                                isLoading = false,
+                                isSuccessful = false,
+                                masjeds = emptyList()
+                            )
                         }
                     }
                 }
-            } catch (e: Exception) {
-                _masjedsUiState.value = MasjedsUIState(
-                    isError = true,
-                    isLoading = false,
-                    isSuccessful = false,
-                    masjeds = listOf(),
-                )
             }
         }
     }
-
 
     fun retryLoadMasjeds() {
         getAllMasjeds()
