d926ad7020a5f01cbc1f6046467555b1c67d22f4
MoatazBadawy <moataz.egy101@gmail.com>
2023-05-28 19:39:12 +0300

    update Image size in masjed item

--- ui/view/src/main/java/com/moataz/masjed/ui/view/screens/components/Image.kt
+++ ui/view/src/main/java/com/moataz/masjed/ui/view/screens/components/Image.kt
@@ -1,10 +1,13 @@
 package com.moataz.masjed.ui.view.screens.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 
 @Composable
@@ -13,7 +16,8 @@ fun ImageLoading(url: String) {
     Image(
         painter = painter,
         contentDescription = null,
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier.fillMaxHeight()
+            .width(160.dp),
         contentScale = ContentScale.FillBounds,
     )
 }
--- ui/view/src/main/java/com/moataz/masjed/ui/view/screens/components/MasjedItem.kt
+++ ui/view/src/main/java/com/moataz/masjed/ui/view/screens/components/MasjedItem.kt
@@ -1,7 +1,6 @@
 package com.moataz.masjed.ui.view.screens.components
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -21,13 +20,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.moataz.masjed.ui.view.R
 import com.moataz.masjed.ui.view.theme.Background
 import com.moataz.masjed.ui.view.theme.ButtonColor
 import com.moataz.masjed.ui.view.theme.TextColor
@@ -104,14 +100,7 @@ fun MasjedItem(
                     horizontalArrangement = Arrangement.End,
                     verticalAlignment = Alignment.CenterVertically,
                 ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.testr),
-                        contentDescription = null,
-                        modifier = Modifier
-                            .fillMaxHeight()
-                            .width(150.dp),
-                        contentScale = ContentScale.FillBounds
-                    )
+                    ImageLoading(url = masjed.masjedImg)
                 }
             }
         }
